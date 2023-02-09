bf687fdd09bfc88520ee4c27d8435396bd1cff9f
Lucas David Erkana <lderkana21x@gmail.com>
2023-02-09 15:37:12 +0200

    Fix icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
+import { AiFillAudio, AiFillHome } from 'react-icons/ai';
+import { FiSettings } from 'react-icons/fi';
 
 const Header = () => (
   <header>
     <Link className="link" to="/">
       {' '}
-      <i className="fa-solid fa-house" />
+      <AiFillHome size={25} />
     </Link>
     <h2 className="header-tag">The World At Your Feet</h2>
     <div className="header-controls">
-      <i className="fa-solid fa-microphone fa-2x" />
-      <i className="fa-solid fa-gear fa-2x" />
+      <AiFillAudio size={25} />
+      <FiSettings size={25} />
     </div>
   </header>
 );
